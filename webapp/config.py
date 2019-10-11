af52a4bda92d890d8f918b2a179133cdd2626b24
Dmitry Bychkov <decast@yandex.ru>
2019-10-11 09:12:53 +0300

    11.10.2019

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WEATHER_DEFAULT_CITY = "Moscow,Russia"
 WEATHER_API_KEY = "3f58b2c6faa7436087d163348192009"
 WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
